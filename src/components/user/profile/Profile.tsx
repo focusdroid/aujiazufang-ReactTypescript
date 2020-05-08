6b238d952bffed9b1d11d94ed5d7d60d634f7491
focusdroid <840254112@qq.com>
2020-05-08 16:40:17 +0800

    新增个人信息修改页面

--- src/components/user/profile/Profile.tsx
+++ src/components/user/profile/Profile.tsx
@@ -1,11 +1,35 @@
 import React, { Fragment } from 'react'
 
 import TitleBar from "../../../plugin/titleBar/TitleBar";
+import img from '../../../static/images/img.jpg'
+const profile = require('../../../style/less/base.module.less')
 
 const Profile = () => {
     return (<Fragment>
         <TitleBar title={'个人信息'}/>
-        <h2>个人信息</h2>
+        <div className={profile.profile}>
+            <div className={profile.userimg}>
+                <div className={profile.profiletitle}>头像</div>
+                <div className={profile.box}>
+                    <div>
+                        <img  className={profile.img} src={img} alt=""/>
+                    </div>
+                    <div className={profile.selectimg}>
+                        <div>选择头像</div>
+                        <div><input type="file"/></div>
+                    </div>
+                </div>
+            </div>
+            <div className={profile.userimg}>
+                <div className={profile.profiletitle}>用户名</div>
+                <div className={profile.box}>
+                    <div><input type="text" placeholder={'用户名'}/></div>
+                </div>
+            </div>
+            <div className={profile.btn}>
+                <button>保存</button>
+            </div>
+        </div>
     </Fragment>)
 }
 
