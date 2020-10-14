c2435e7c3db4c67837f663fb095a4e19e2b76d62
focusdroid <840254112@qq.com>
2020-10-14 23:47:43 +0800

    删除部分请求数据

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,9 +1,5 @@
 import fetch from './fetch'
 
-export function TestFetch (obj:Object = {}) {
-    console.log(fetch.requestget('https://api.github.com', obj))
-    return fetch.requestget('https://api.github.com', obj)
-}
 export function get_image_code (obj:Object = {}) {
     return fetch.requestget(`/api/v1.0/get_image_code`, obj)
 }
